07c8cfc5b28f6325d839294ddd090edbae7a1f8c
Максим Парфенюк <drake@MacBook-Pro-Maksim.local>
2024-02-12 13:50:01 +0200

    second commit of parsing site

--- parsing.py
+++ parsing.py
@@ -1,26 +1,42 @@
+
+
+
 import requests
-from bs4 import BeautifulSoup
-import os
 from urllib.parse import urljoin
+import bs4
+import os
 
-# Request the webpage
-url = "https://gocsgo.net/guides/advice/avatarki-dlya-doty/"
-response = requests.get(url)
+request = requests.get("https://gocsgo.net/guides/advice/avatarki-dlya-doty/")
 
-# Check if request was successful
-if response.status_code == 200:
-    # Parse the HTML content
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Find all image tags
-    images = soup.find_all('img')
-    
-    # Create a directory to save images if it doesn't exist
-    if not os.path.exists('study/images'):
-        os.makedirs('study/images')
+beautifull_soup = bs4.BeautifulSoup(request.text, 'html.parser')
 
-    # Download and save each image
-    for img in images:
+# find_site = beautifull_soup.find("class")
+
+site = (beautifull_soup.find('body', class_ = "post-template-default single single-post postid-14669 single-format-standard custom-background wp-custom-logo boxed-layout")).find('div', class_ = 'hfeed site')
+# print(site)
+site_content = site.find('div', id = 'content')
+
+site_container_clearfix = site_content.find('div', class_ = 'container clearfix')
+# print(site_container_clearfix)
+site_primary = site_container_clearfix.find('div', id = 'primary')
+
+site_main = site_primary.find('main', class_ = 'site-main clearfix test')
+
+site_entry_content_clearfix = site_main.find('div', class_ = 'entry-content clearfix')
+
+site_all_photoes = site_entry_content_clearfix.find('tbody')
+# h = site_all_photoes.find('td')
+# photo = h.find('a').find_all('img')
+photo = site_all_photoes.find_all('img')
+print(photo)
+# img_name = os.path.basename(photo)
+# img_path = os.path.join('study/images', img_name)
+if request.status_code == 200:
+    print('site work normally')
+else:
+    print("some troubles with site")
+print(request)
+for img in photo:
         try:
             img_url = img.get('src')  # Get the 'src' attribute of the image tag
             if img_url:
@@ -28,7 +44,7 @@ if response.status_code == 200:
                 img_url = urljoin("https://gocsgo.net/guides/advice/avatarki-dlya-doty/", img_url)
                 img_name = os.path.basename(img_url)
                 img_path = os.path.join('study/images', img_name)
-                
+
                 # Download the image
                 img_data = requests.get(img_url).content
 
@@ -38,5 +54,3 @@ if response.status_code == 200:
                     print(f"Image '{img_name}' saved successfully.")
         except requests.exceptions.ConnectionError:
             print(f"Connection error occurred while downloading image '{img_url}'. Skipping...")
-else:
-    print("Failed to retrieve the webpage.")
